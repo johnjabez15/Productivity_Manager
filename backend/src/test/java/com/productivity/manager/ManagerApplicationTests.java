ce10065526ac481528a45b4ea0027c2d43d59298
johnjabez15 <johnjabezbusiness@gmail.com>
2025-08-31 23:14:04 +0530

    initial backend files added!

---
+++ backend/src/test/java/com/productivity/manager/ManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productivity.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
